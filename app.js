90cb169b245b0d17c707ff6d0fadffe40df933d6
Renato Kuc <renatokuc@innovacxion.com>
2023-10-17 12:21:25 -0500

    adding package json

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hola mundo")
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (request, response) => {
+  response.send('Testing Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Test app listening at http://localhost:${port}`)
+})
